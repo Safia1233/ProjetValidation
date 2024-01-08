7767bc4bf23cd611e8e03ecfe8545d64f0b07259
Fezai Safia <fezai.safia.itsd@gmail.com>
2024-01-08 19:25:59 +0100

    Some Modifications: note.ts/cours.service.ts

--- src/app/components/note/note.component.ts
+++ src/app/components/note/note.component.ts
@@ -10,45 +10,41 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./note.component.css']
 })
 export class NoteComponent implements OnInit {
-  addFormNote!:FormGroup;
-  note:any={};
-   students:any=[];
-   cours:any=[];
+  addFormNote!: FormGroup;
+  note: any = {};
+  students: any = [];
+  cours: any = [];
 
-   studentId:any;
-   courId:any;
-  constructor( private noteService:NoteService,
-    private userService:UserService,
-     private courService:CourService) { }
+  studentId: any;
+  courId: any;
+  constructor(private noteService: NoteService,
+    private userService: UserService,
+    private courService: CourService) { }
 
   ngOnInit(): void {
-    this.courService.getAllCours().subscribe((data)=>{
-    console.log("here data from BE",data.cours);
-    this.cours = data.cours
+    this.courService.getAllCours().subscribe((data) => {
+      this.cours = data.cours
     });
-    this.userService.getAllUsers().subscribe((response)=>{
-      console.log("here data from BE",response.users);
-    this.students=response.users.filter((user:any)=> user.role=='student');
+    this.userService.getAllUsers().subscribe((response) => {
+      this.students = response.users.filter((user: any) => user.role == 'student');
     })
   }
-  addNote(){
-    console.log("here note",this.note);
-    
-   this.note.idCour=this.courId;
-   this.note.idStudent= this.studentId
-   
-  this.noteService.addNote(this.note).subscribe((response)=>{
-    console.log("here response from BE",response.msg);
-    
-  })
+  addNote() {
+    this.note.idCour = this.courId;
+    this.note.idStudent = this.studentId
+
+    this.noteService.addNote(this.note).subscribe((response) => {
+      console.log("here response from BE", response.msg);
+
+    })
   }
   selectStudents(evt: any) {
     console.log("here event", evt.target.value);
-   this.studentId = evt.target.value
+    this.studentId = evt.target.value
   }
 
   selectCour(evt: any) {
     console.log("here event", evt.target.value);
-   this.courId = evt.target.value
+    this.courId = evt.target.value
   }
 }
--- src/app/services/cour.service.ts
+++ src/app/services/cour.service.ts
@@ -5,36 +5,36 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CourService {
-courUrl :string="http://localhost:3000/cours"
-  constructor(private httpClient:HttpClient) { }
- 
-getAllCours(){
-  return this.httpClient.get<{cours:any}>(this.courUrl)
-}
+  courUrl: string = "http://localhost:3000/cours"
+  constructor(private httpClient: HttpClient) { }
+
+  getAllCours() {
+    return this.httpClient.get<{ cours: any }>(this.courUrl);
+  }
 
 
-getCourById(id:any){
-  return this.httpClient.get<{cour:any}>(`${this.courUrl}/${id}`)
-}
+  getCourById(id: any) {
+    return this.httpClient.get<{ cour: any }>(`${this.courUrl}/${id}`);
+  }
 
-addCour(obj:any,img:File){
- let formData = new FormData();
- formData.append("nameCour",obj.nameCour);
- formData.append("description",obj.description);
- formData.append("dure",obj.dure);
- formData.append("teacher",obj.teacher);
- formData.append("img",img);
- return this.httpClient.post<{msg:string}>(this.courUrl,formData)
-}
+  addCour(obj: any, img: File) {
+    let formData = new FormData();
+    formData.append("nameCour", obj.nameCour);
+    formData.append("description", obj.description);
+    formData.append("dure", obj.dure);
+    formData.append("teacher", obj.teacher);
+    formData.append("img", img);
+    return this.httpClient.post<{ msg: string }>(this.courUrl, formData);
+  }
 
- editCour(obj:any){
-   return this.httpClient.put<{isUpdated:boolean}>(this.courUrl,obj)
- }
- deleteCour(id:any){
-   return this.httpClient.delete<{msg:string}>(`${this.courUrl}/${id}`)
- }
- getDetailsCourById(courId:any){
-  return this.httpClient.get<{cour:any}>(`${this.courUrl}/details/${courId}`)
-}
- 
+  editCour(obj: any) {
+    return this.httpClient.put<{ isUpdated: boolean }>(this.courUrl, obj);
+  }
+  deleteCour(id: any) {
+    return this.httpClient.delete<{ msg: string }>(`${this.courUrl}/${id}`);
+  }
+  getDetailsCourById(courId: any) {
+    return this.httpClient.get<{ cour: any }>(`${this.courUrl}/details/${courId}`)
+  }
+
 }
